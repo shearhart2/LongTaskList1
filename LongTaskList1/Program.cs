20642947aa4c51e5d0515c3e0b5f5ddb4c030d95
Brent Shearhart <shearhart2@gmail.com>
2019-03-07 10:27:59 -0500

    If committed, unfinished version from 10:27 AM thursday March 7th will show

--- LongTaskList1/Program.cs
+++ LongTaskList1/Program.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 /// <summary>
-///  //Console.ForegroundColor = ConsoleColor.DarkGray;
-//Console.WriteLine("this should be dark gray");
-//Console.ForegroundColor = ConsoleColor.Red;
-//Console.WriteLine("this should be dark red");
+/// Just successfully saved, and opened file with completed tasks grayed out. Need to verify that the
+/// re-enter and the save methods work, make lists 25 items each, complete options menu and make it present after each action taken by user.
+/// 
 /// </summary>
+
 namespace LongTaskList1
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
 
-            //Intro to task list
-            Console.WriteLine("Long Task List");
-            //Create List
+            ShowTaskList();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             List<string> Page1 = new List<string>();
             //Add first task
-            Console.WriteLine("Please enter a new task to begin your list.");
-            AddTask(Console.ReadLine(), Page1);
-            Console.WriteLine("Please enter a new task");
-            AddTask(Console.ReadLine(), Page1);
-            Console.WriteLine("Please enter a new task.");
-            AddTask(Console.ReadLine(), Page1);
-            Console.WriteLine("Please enter a new task.");
-            AddTask(Console.ReadLine(), Page1);
-            ShowTaskList(Page1);
-            SelectTask(Page1);
-            ShowTaskList(Page1);
-            
-            
-            
+
+
+
+
+
+            Save(Page1);
+        }
+
+        private static void Save(List<string> pageNumber)
+        {
+            File.WriteAllLines("LongTaskList1.txt", pageNumber);
+            Console.WriteLine("Your list has been saved");
         }
 
         private static void AddTask(string userInput, List<string> pageNumber)
@@ -43,44 +41,42 @@ namespace LongTaskList1
             pageNumber.Add(userInput);
             Console.Clear();
             Console.WriteLine($"{userInput} has been added to your Task List");
-            
+
         }
 
-        private static void ShowTaskList(List<string> pageNumber)
+        private static void ShowTaskList()
         {
             Console.Clear();
-            foreach (string userInput in pageNumber)
+            string[] currentTaskList = File.ReadAllLines("LongTaskList1.txt");//opens saved file
+            foreach (string item in currentTaskList)
             {
-                Console.WriteLine($"{pageNumber.IndexOf(userInput)}. {userInput}");
+                if (item.Contains($"-*-"))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+                }
+
             }
+
+        }
+
+        
+
+        private static void TaskAction(List<string> pageNumber)
+        {
             
-        }
-
-        private static void DeleteTask(List<string> pageNumber)
-        {
-            ///Working on color change. This method will currently delete an item and then replace it in the same index.
-            ///You were attempting to change the color between these two steps, but it doesn't quite work.
-            ///Items with the comments are there for testing purposes only
-            ///Try making color change in a method and calling it on a string
-            ///***this deletion and replacement can be used for moving the re-entered tasks as well***
-            ShowTaskList(pageNumber);
-            Console.WriteLine("Please enter the number for the item you would like to delete. ");
-            int elementNumber = int.Parse(Console.ReadLine());
-            string placeHolder = pageNumber.ElementAt(elementNumber);
-            pageNumber.RemoveAt(elementNumber);
-            ShowTaskList(pageNumber);//
-            Console.ReadLine();
-            pageNumber.Insert(elementNumber, placeHolder);
-            ShowTaskList(pageNumber);//
 
 
-        }
-
-        private static string TaskAction(List<string> pageNumber)
-        {
             while (true)
             {
-                Console.WriteLine($"What would you like to do with {SelectTask(pageNumber)}?" +
+                string choice = SelectTask(pageNumber);
+                int selection = pageNumber.IndexOf(choice);
+                Console.WriteLine($"What would you like to do with {choice}?" +
                               $"\n\t1. Mark as Complete" +
                               $"\n\t2. Re-Enter" +
                               $"\n\t3. Return to Task List");
@@ -88,16 +84,18 @@ namespace LongTaskList1
                 if (userInput == "1")
                 {
                     //Mark As Complete Method Here
-                    return "Complete";
+                    GrayOut(pageNumber, selection);
+                    break;
+                    
                 }
                 else if (userInput == "2")
                 {
                     //Re-Enter Method Here
-                    return "Re-enter";
+                    ReEnter(pageNumber, selection);
                 }
                 else if (userInput == "3")
                 {
-                    ShowTaskList(pageNumber);
+                    ShowTaskList();
                 }
                 else
                 {
@@ -105,13 +103,16 @@ namespace LongTaskList1
                     Console.ReadLine();
                 }
             }
+            //Console.WriteLine("It worked");// this is for testing and needs to be removed and fixed to where it either returns something or is void
         }
+
+
 
         private static string SelectTask(List<string> pageNumber)
         {
             while (true)
             {
-                ShowTaskList(pageNumber);//Make this entire method an option in the Show task method
+
                 Console.WriteLine("Please enter the number for the item you would like to select. ");
                 try
                 {
@@ -144,26 +145,10 @@ namespace LongTaskList1
                 Console.ReadLine();
             }
         }
-        ///Items with the comments are there for testing purposes only
-        private static void ReEnterTask(List<string> pageNumber)
-        {
-            
-            ShowTaskList(pageNumber);
-            Console.WriteLine("Please enter the number for the item you would like to delete. ");
-            int elementNumber = int.Parse(Console.ReadLine());
-            string placeHolder = pageNumber.ElementAt(elementNumber);
-            pageNumber.RemoveAt(elementNumber);
-            ShowTaskList(pageNumber);//
-            Console.ReadLine();
-            pageNumber.Insert(elementNumber, placeHolder);//this should replace the grayed out version back where it was **after we figure out the gray**
-            pageNumber.Add(placeHolder);//this should re-enter the non-grayed out version to the end of the list **after we figure out the gray**
-            ShowTaskList(pageNumber);//
-        }
 
-        /// <summary>
-        /// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        /// </summary>
-        /// <param Page1="pageNumber"></param>
+        ///Items with the comments are there for testing purposes only
+        
+        
         private static void OptionsMenu(List<string> pageNumber)
         {
             Console.WriteLine("OPTIONS MENU:" +
@@ -174,14 +159,14 @@ namespace LongTaskList1
 
             while (true)
             {
-                
+
                 string userInput = Console.ReadLine();
                 if (userInput == "1")
                 {
                     SelectTask(pageNumber);
                 }
                 else if (userInput == "2")
-                { 
+                {
                     //AddTask 
                 }
                 else if (userInput == "3")
@@ -195,6 +180,67 @@ namespace LongTaskList1
                 }
             }
         }
+        
+
+        private static void GrayOut(List<string> pageNumber, int selection)
+        {
+            pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
+            pageNumber.RemoveAt(selection + 1);
+
+            foreach (string item in pageNumber)  
+            {
+                
+                if (pageNumber.IndexOf(item) == selection)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                }
+
+            }
+            File.WriteAllLines("LongTaskList1.txt", pageNumber);
+        }
+
+
+        private static void ReEnter(List<string> pageNumber, int selection)
+        {
+            //pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
+            //pageNumber.RemoveAt(selection + 1);
+
+            foreach (string item in pageNumber)
+            {
+                pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
+                pageNumber.Add(pageNumber.ElementAt(selection+1));
+                pageNumber.RemoveAt(selection + 1);
+                if (pageNumber.IndexOf(item) == selection)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                }
+
+            }
+            File.WriteAllLines("LongTaskList1.txt", pageNumber);
+        }
+        //private static string DeleteTask(string item)
+        //{
+        //    string deletedItem = $"-{item}-";
+        //    return deletedItem;
+        //}
+
     }
 }
 
+//int elementNumber = int.Parse(Console.ReadLine());
+//return pageNumber.ElementAt(elementNumber);
