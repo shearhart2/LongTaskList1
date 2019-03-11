1ba06d677d2c7815113b5cab32aae65c2381f434
Brent Shearhart <shearhart2@gmail.com>
2019-03-11 10:36:44 -0400

    will commit application that is functional, but not exactly pretty

--- LongTaskList1/Program.cs
+++ LongTaskList1/Program.cs
@@ -5,9 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 /// <summary>
-/// Just successfully saved, and opened file with completed tasks grayed out. Need to verify that the
-/// re-enter and the save methods work, make lists 25 items each, complete options menu and make it present after each action taken by user.
-/// 
+///
 /// </summary>
 
 namespace LongTaskList1
@@ -18,84 +16,95 @@ namespace LongTaskList1
         static void Main(string[] args)
         {
 
-            ShowTaskList();
+            
             Console.ForegroundColor = ConsoleColor.Yellow;
             List<string> Page1 = new List<string>();
-            //Add first task
+            Page1.Capacity = 25;
+            OptionsMenu(Page1);
+            
 
-
-
-
-
-            Save(Page1);
         }
 
         private static void Save(List<string> pageNumber)
         {
             File.WriteAllLines("LongTaskList1.txt", pageNumber);
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Your list has been saved");
+            Console.ForegroundColor = ConsoleColor.Yellow;
         }
 
-        private static void AddTask(string userInput, List<string> pageNumber)
+        private static void AddTask(List<string> pageNumber)
         {
+            Console.WriteLine("Enter a task");
+            string userInput = Console.ReadLine();
             pageNumber.Add(userInput);
             Console.Clear();
             Console.WriteLine($"{userInput} has been added to your Task List");
-
+            Save(pageNumber);
         }
 
-        private static void ShowTaskList()
+        private static void ShowTaskList(List<string> pageNumber)
         {
             Console.Clear();
-            string[] currentTaskList = File.ReadAllLines("LongTaskList1.txt");//opens saved file
+            string[] currentTaskList = File.ReadAllLines("LongTaskList1.txt");
             foreach (string item in currentTaskList)
             {
-                if (item.Contains($"-*-"))
+                if(!pageNumber.Contains(item))
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+                    pageNumber.Add(item);
                 }
-                else
+                
+                if(pageNumber.IndexOf(item)<25)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+                    if (item.Contains($"-*-"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{Array.IndexOf(currentTaskList, item)}. {item}");
+
+                    }
                 }
+                
 
             }
-
+            Save(pageNumber);
+            PageSelect(pageNumber);
         }
 
         
 
         private static void TaskAction(List<string> pageNumber)
         {
-            
-
-
             while (true)
             {
+                
                 string choice = SelectTask(pageNumber);
                 int selection = pageNumber.IndexOf(choice);
                 Console.WriteLine($"What would you like to do with {choice}?" +
                               $"\n\t1. Mark as Complete" +
                               $"\n\t2. Re-Enter" +
-                              $"\n\t3. Return to Task List");
+                              $"\n\t3. Return to Options Menu");
                 string userInput = Console.ReadLine();
                 if (userInput == "1")
                 {
-                    //Mark As Complete Method Here
                     GrayOut(pageNumber, selection);
                     break;
                     
                 }
                 else if (userInput == "2")
                 {
-                    //Re-Enter Method Here
                     ReEnter(pageNumber, selection);
+                    break;
                 }
                 else if (userInput == "3")
                 {
-                    ShowTaskList();
+                    OptionsMenu(pageNumber);
+                    break;
                 }
                 else
                 {
@@ -103,7 +112,7 @@ namespace LongTaskList1
                     Console.ReadLine();
                 }
             }
-            //Console.WriteLine("It worked");// this is for testing and needs to be removed and fixed to where it either returns something or is void
+            Save(pageNumber);
         }
 
 
@@ -113,7 +122,7 @@ namespace LongTaskList1
             while (true)
             {
 
-                Console.WriteLine("Please enter the number for the item you would like to select. ");
+                Console.WriteLine("Please enter the number for the item you would like to select, or press enter to return to the options menu. ");
                 try
                 {
                     int elementNumber = int.Parse(Console.ReadLine());
@@ -123,64 +132,218 @@ namespace LongTaskList1
                 catch (FormatException)
                 {
                     Console.Clear();
-                    Console.WriteLine("Your entry was not valid. Please press enter and try again.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    OptionsMenu(pageNumber);
                 }
                 catch (OverflowException)
                 {
                     Console.Clear();
-                    Console.WriteLine("Your entry was not valid. Please press enter and try again.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    OptionsMenu(pageNumber);
                 }
                 catch (ArgumentNullException)
 
                 {
                     Console.Clear();
-                    Console.WriteLine("Your entry was not valid. Please press enter and try again.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    OptionsMenu(pageNumber);
                 }
                 catch (Exception)
 
                 {
                     Console.Clear();
-                    Console.WriteLine("Your entry was not valid. Please press enter and try again.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    OptionsMenu(pageNumber);
                 }
                 Console.ReadLine();
             }
         }
 
-        ///Items with the comments are there for testing purposes only
+        
         
         
         private static void OptionsMenu(List<string> pageNumber)
         {
-            Console.WriteLine("OPTIONS MENU:" +
+
+            while (true)
+            {
+                
+                Console.WriteLine("\nOPTIONS MENU:" +
                 "\n\n\t1. Select Task" +
                   "\n\t2. Add Task" +
-                  "\n\t3. Delete Task");
-            //Not sure if Next Page belongs here of if it should be its own place on each list
+                  "\n\t3. Delete Task" +
+                  "\n\t4. Go to Task List" +
+                  "\n\t5. Exit Program");
+                string userInput = Console.ReadLine();
+                try
+                {
+                    if (userInput == "1")
+                    {
+                        TaskAction(pageNumber);
+                    }
+                    else if (userInput == "2")
+                    {
+                        AddTask(pageNumber);
+                    }
+                    else if (userInput == "3")
+                    {
+                        Console.WriteLine("Please enter the number for the item you would like to delete, or press enter to reurn to the options menu.");
+                        int selection = int.Parse(Console.ReadLine());
+                        GrayOut(pageNumber, selection);
+                    }
+
+
+
+                    else if (userInput == "4")
+                    {
+                        ShowTaskList(pageNumber);
+                    }
+                    else if (userInput == "5")
+                    {
+                        break;
+                    }
+                    else
+                    {
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nYour entry was not valid, please try again");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    
+                }
+            }
+            
+        }
+
+        private static void PageSelect(List<string> pageNumber)
+        {
 
             while (true)
             {
 
+                Console.WriteLine("\nOPTIONS MENU:" +
+                
+                  "\n\t1. Go to Page 2 of Task List" +
+                  "\n\t2. Go to Page 3 of Task List" +
+                  "\n\t3. Go to Page 4 of Task List" +
+                  "\n\t4. Go to Options Menu");
                 string userInput = Console.ReadLine();
-                if (userInput == "1")
+                try
                 {
-                    SelectTask(pageNumber);
+                    
+                    if (userInput == "1")
+                    {
+                        Console.Clear();
+                        foreach (string item in pageNumber)
+                        {
+
+                            if (pageNumber.IndexOf(item) >= 25 && pageNumber.IndexOf(item) < 50)
+                            {
+                                if (item.Contains("-*-"))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                }
+                            }
+
+                        }
+                    }
+                    else if (userInput == "2")
+                    {
+                        Console.Clear();
+                        foreach (string item in pageNumber)
+                        {
+
+                            if (pageNumber.IndexOf(item) >= 50 && pageNumber.IndexOf(item) < 75)
+                            {
+                                if (item.Contains("-*-"))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                }
+                            }
+
+                        }
+                    }
+                    else if (userInput == "3")
+                    {
+                        Console.Clear();
+                        foreach (string item in pageNumber)
+                        {
+
+                            if (pageNumber.IndexOf(item) >= 75)
+                            {
+                                if (item.Contains("-*-"))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                                }
+                            }
+
+                        }
+                    }
+                    else if (userInput == "4")
+                    {
+                        break;
+                    }
+                    else
+                    {
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nYour entry was not valid, please try again");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
                 }
-                else if (userInput == "2")
+                catch (Exception)
                 {
-                    //AddTask 
-                }
-                else if (userInput == "3")
-                {
-                    //Delete Task
-                }
-                else
-                {
-                    Console.WriteLine("Your entry was not valid. Please press enter and try again.");
-                    Console.ReadLine();
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are being re-routed to the options menu.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
                 }
             }
+
         }
-        
 
         private static void GrayOut(List<string> pageNumber, int selection)
         {
@@ -189,58 +352,57 @@ namespace LongTaskList1
 
             foreach (string item in pageNumber)  
             {
-                
-                if (pageNumber.IndexOf(item) == selection)
+
+                if (pageNumber.IndexOf(item) < 25)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                    if (item.Contains("-*-"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                    }
                 }
 
             }
-            File.WriteAllLines("LongTaskList1.txt", pageNumber);
         }
 
 
         private static void ReEnter(List<string> pageNumber, int selection)
         {
-            //pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
-            //pageNumber.RemoveAt(selection + 1);
 
+            pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
+            pageNumber.Add(pageNumber.ElementAt(selection + 1));
+            pageNumber.RemoveAt(selection + 1);
             foreach (string item in pageNumber)
             {
-                pageNumber.Insert(selection, ($"{pageNumber.ElementAt(selection)}-*-"));
-                pageNumber.Add(pageNumber.ElementAt(selection+1));
-                pageNumber.RemoveAt(selection + 1);
-                if (pageNumber.IndexOf(item) == selection)
+                if(pageNumber.IndexOf(item)<25)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    
+                    if (item.Contains("-*-"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
 
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
-                }
 
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{pageNumber.IndexOf(item)}. {item}");
+                    }
+                }
+               
             }
-            File.WriteAllLines("LongTaskList1.txt", pageNumber);
         }
-        //private static string DeleteTask(string item)
-        //{
-        //    string deletedItem = $"-{item}-";
-        //    return deletedItem;
-        //}
+        
 
     }
 }
 
-//int elementNumber = int.Parse(Console.ReadLine());
-//return pageNumber.ElementAt(elementNumber);
